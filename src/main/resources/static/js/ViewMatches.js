81f92c05374015df11c2626f6c8bb0f6e6f45385
shushanth <sushanth179@gmail.com>
2023-06-22 16:21:40 +0530

    added left arrow for less matches btn

--- src/main/resources/static/js/ViewMatches.js
+++ src/main/resources/static/js/ViewMatches.js
@@ -1,8 +1,11 @@
 window.onload = () => {
     const matchTypesSize = document.getElementById("matchTypesSize").innerText;
-    const svgArrow = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill svgArrow" viewBox="0 0 16 16">
+    const svgArrowRight = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill svgArrow" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                         </svg>`;
+    const svgArrowLeft = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                                 <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                </svg>`;
     for (let i=0 ; i<matchTypesSize; i++){
         let matchCards = document.getElementsByClassName("matchCard"+i);
         let moreMatchesBtn = document.getElementById("ViewMoreMatches"+i);
@@ -45,12 +48,12 @@ window.onload = () => {
                     matchCards[j].classList.remove("showCard");
                     matchCards[j].classList.add("hideCard");
                 }
-                moreMatchesBtn.innerHTML="More Matches    "+svgArrow;
+                moreMatchesBtn.innerHTML="More Matches    "+svgArrowRight;
                 moreMatches = true;
             }
 
             if(hideCardCount === 0 && !moreMatches){
-                moreMatchesBtn.innerHTML="Less Matches    "+svgArrow;
+                moreMatchesBtn.innerHTML="Less Matches    "+svgArrowLeft;
             }
         })
     }
