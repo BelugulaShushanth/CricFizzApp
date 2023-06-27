4dab26ceba1f821e905c18e95049b8d31a52a4b0
shushanth <sushanth179@gmail.com>
2023-06-27 08:21:21 +0530

    Implemented Responsive Accordian, Cards, Footer for mobile view

--- src/main/resources/static/js/ViewMatches.js
+++ src/main/resources/static/js/ViewMatches.js
@@ -2,35 +2,45 @@ window.onload = () => {
     activateDeactivateNavBtns();
     adjustNoOfCards();
     const matchTypesSize = document.getElementById("matchTypesSize").innerText;
+
+    hideHeaderChars();
+    let width = screen.width;
+    if(width<1000){
+        defaultNoOfMatches(matchTypesSize,4);
+        btnMoreMatches(matchTypesSize,4);
+    }
+    else{
+        defaultNoOfMatches(matchTypesSize,6);
+        btnMoreMatches(matchTypesSize,6);
+    }
+
+
+}
+
+function defaultNoOfMatches(matchTypesSize,defaultSize){
+    for (let i=0 ; i<matchTypesSize; i++){
+        let matchCards = document.getElementsByClassName("matchCard"+i);
+        let moreMatchesBtn = document.getElementById("ViewMoreMatches"+i);
+        if(matchCards.length<=defaultSize){
+            moreMatchesBtn.style.display = "none";
+        }
+        else{
+            moreMatchesBtn.style.display = "";
+            for (let j =  defaultSize; j<matchCards.length; j++ ) {
+                matchCards[j].classList.remove("showCard");
+                matchCards[j].classList.add("hideCard");
+            }
+        }
+    }
+}
+
+function btnMoreMatches(matchTypesSize,defaultSize){
     const svgArrowRight = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill svgArrow" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                         </svg>`;
     const svgArrowLeft = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                                  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                                 </svg>`;
-
-    for (let i=0 ; i<matchTypesSize; i++){
-        let matchCards = document.getElementsByClassName("matchCard"+i);
-        let moreMatchesBtn = document.getElementById("ViewMoreMatches"+i);
-        if(matchCards.length<=6){
-            moreMatchesBtn.style.display = "none";
-        }
-        else{
-            moreMatchesBtn.style.display = "";
-            for (let j =  6; j<matchCards.length; j++ ) {
-                matchCards[j].classList.remove("showCard");
-                matchCards[j].classList.add("hideCard");
-            }
-        }
-    }
-
-    const cardHeaderLimit = 40;
-    let cardHeader = document.getElementsByClassName("cardHeaderClass");
-    for (let i=0; i<cardHeader.length; i++){
-        if(cardHeader[i].innerText.length > 40) {
-            cardHeader[i].innerText = cardHeader[i].innerText.substring(0, cardHeaderLimit) + "...";
-        }
-    }
 
     for (let i=0 ; i<matchTypesSize; i++){
         let moreMatchesBtn = document.getElementById("ViewMoreMatches"+i);
@@ -43,7 +53,7 @@ window.onload = () => {
                     matchCards[j].classList.add("showCard");
                     hideCardCount ++;
                 }
-                if(hideCardCount >= 6){
+                if(hideCardCount >= defaultSize){
                     break;
                 }
             }
@@ -55,7 +65,7 @@ window.onload = () => {
             }
             let moreMatches = false;
             if(moreMatchesBtn.innerText.trim() === "Less Matches"){
-                for (let j =  6; j<matchCards.length; j++ ) {
+                for (let j =  defaultSize; j<matchCards.length; j++ ) {
                     matchCards[j].classList.remove("showCard");
                     matchCards[j].classList.add("hideCard");
                 }
@@ -68,7 +78,17 @@ window.onload = () => {
             }
         })
     }
-
+}
+function hideHeaderChars() {
+    const cardHeaderLimit = 30;
+    let matchSeriesName = document.getElementsByClassName("matchSeriesName");
+    let matchDesc = document.getElementsByClassName("matchDesc");
+    for (let i=0; i<matchSeriesName.length; i++){
+        let size = matchSeriesName[i].innerText.length+matchDesc[i].innerText.length;
+        if( size > cardHeaderLimit) {
+            matchSeriesName[i].innerText = matchSeriesName[i].innerText.substring(0, cardHeaderLimit) + "...";
+        }
+    }
 }
 
 function activateDeactivateNavBtns(){
