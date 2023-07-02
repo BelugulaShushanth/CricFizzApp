6ca75d29f4bd0dcaea6032173568248d11a37ca6
shushanth <sushanth179@gmail.com>
2023-07-02 12:59:47 +0530

    Implemented Responsive UI for ViewMatchCommentary.html

--- src/main/resources/static/js/ViewMatchCommentary.js
+++ src/main/resources/static/js/ViewMatchCommentary.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     activateDeactivateNavBtns();
+    adjustCards();
 }
 function activateDeactivateNavBtns(){
     let live = document.getElementById("live");
@@ -13,4 +14,24 @@ function activateDeactivateNavBtns(){
 
     upcoming.classList.remove("activateNavBtn");
     upcoming.classList.add("deactivateNavBtn");
+}
+
+function adjustCards(){
+    let width = screen.width;
+    let scores = document.getElementsByClassName("scores")[0];
+    let keystats = document.getElementsByClassName("keystats")[0];
+    if(width<=1000){
+        scores.classList.add("col-md-12");
+        scores.classList.add("col-sm-12");
+
+        keystats.classList.add("col-md-12");
+        keystats.classList.add("col-sm-12");
+    }
+    else{
+        scores.classList.remove("col-md-12");
+        scores.classList.remove("col-sm-12");
+
+        keystats.classList.remove("col-md-12");
+        keystats.classList.remove("col-sm-12");
+    }
 }
