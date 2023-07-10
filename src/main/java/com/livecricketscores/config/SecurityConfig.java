bb95be072ec31f5f7b99ffa1b6466815804f8ef5
shushanth <sushanth179@gmail.com>
2023-07-10 22:10:19 +0530

    Implemented google OAuth Login and Logout

--- src/main/java/com/livecricketscores/config/SecurityConfig.java
+++ src/main/java/com/livecricketscores/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.livecricketscores.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,10 +14,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeRequests( requests -> requests
-                .antMatchers("/","/matches/**","/getMatches/**","/getMatch/**,/getMatchScoreCard/**"
-                        ,"/error", "/resources/**","/static/**", "/css/**", "/js/**", "/images/**").permitAll()
+                .antMatchers("/matches/**","/getMatches/**","/getMatch/**,/getMatchScoreCard/**"
+                        ,"/error", "/resources/**","/static/**", "/css/**", "/js/**", "/images/**"
+                        ,"/fonts/**","/vendor/**"
+                        ,"/loginUser").permitAll()
                 .anyRequest().authenticated()
-        );
+        )
+        .oauth2Login()
+        .defaultSuccessUrl("/loggedInUser")
+        .failureUrl("/")
+                .and()
+                .logout()
+                .logoutUrl("/logoutUser")
+                .logoutSuccessUrl("/logoutSuccess");
 
         return httpSecurity.build();
     }
