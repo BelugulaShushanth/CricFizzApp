323e7086d637a83d61a23fdde49c3d57dbd5d14c
shushanth <sushanth179@gmail.com>
2023-07-12 09:35:28 +0530

    Implemented Eureka Client

--- src/main/java/com/cricFizzApp/CricFizzApplication.java
+++ src/main/java/com/cricFizzApp/CricFizzApplication.java
@@ -2,8 +2,10 @@ package com.cricFizzApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CricFizzApplication {
 
 	public static void main(String[] args) { SpringApplication.run(CricFizzApplication.class, args);}
